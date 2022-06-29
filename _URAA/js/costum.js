bf29cd601f7df76ebf9c28d2d6773f18514ba47f
Fajar Alam <sidik3998@gmail.com>
2022-06-29 20:56:01 +0700

    first commit

---
+++ _URAA/js/costum.js
@@ -0,0 +1,76 @@
+/*
+  By Difa WRD 10121916
+  refrensi https://www.peteranswers.com/id/
+*/
+
+//by kang jar backsound music
+const audio = new Audio("./_URAA/audio/BACKSOUND.mp3");
+$("body").click(function () {
+  audio.play();
+});
+
+// enabled elemen inputan
+function setEnable() {
+  $("input").removeAttr("disabled");
+  $("select").removeAttr("disabled");
+  $("textarea").removeAttr("disabled");
+  $("button").removeAttr("disabled");
+  $(".close").show();
+}
+
+// disabled element inputan
+function setDisable() {
+  $("input").attr("disabled", true);
+  $("select").attr("disabled", true);
+  $("textarea").attr("disabled", true);
+  $("button").attr("disabled", true);
+  $(".close").hide();
+}
+
+// menampilkan error text setelah element yang di definisikan beserta pesan nya
+function ShowErrText(element, msg) {
+  $(".text-eror").remove();
+  $(element).after('<div class="text-eror">' + msg + "</div>");
+  $(".text-eror").hide().fadeIn(600);
+  $(element).focus();
+}
+
+// Memeutar audio
+function UraaHaHaHa() {
+  const audio = new Audio("./_URAA/audio/URAHAHAHA.mp3");
+  audio.play();
+}
+
+// Animasi Loading & menyembunyikan elemet (def), judul loading (def), timer loading (def), status (def)
+function loadingduls(element, titleloading, timer, status) {
+  setDisable();
+  $(element).hide();
+  $(".progresbar").remove();
+  $(element).after(
+    '<div class="progresbar"> <div class="counter">0</div> <div id="loading"> <span id="loading-inside"></span> </div> </div>'
+  );
+  var progressbar = $(".progresbar");
+  var counter = $(".counter");
+  var barInside = $("#loading-inside");
+  var interval = setInterval(LetsLoading, timer);
+  var progress = 0;
+
+  progressbar.show(500);
+
+  function LetsLoading() {
+    if (progress >= 100) {
+      progressbar.hide();
+      $(element).fadeIn(500);
+      if (status) {
+        UraaHaHaHa();
+      }
+      clearInterval(interval);
+      $(".progresbar").remove();
+      setEnable();
+    } else {
+      progress += 1;
+      counter.text("" + titleloading + " " + progress + "%");
+      barInside.css("width", progress + "%");
+    }
+  }
+}
---
+++ _URAA/js/ramalankematian.js
@@ -0,0 +1,123 @@
+/*
+  By Fajar 10121919 & Heri 10121914
+  Modif By Difa WRD 10121916 
+*/
+
+$("#ramalan-kematian").ready(function () {
+  $("#ramalkematiakulagi").click(function () {
+    $("#hasil-ramalan-kematian").fadeOut(500);
+    $("#ramalan-kematian").fadeIn(500);
+  });
+
+  $(".tahun_lahir").keyup(function () {
+    $(this).val($(this).val().replace(/\D/g, ""));
+  });
+
+  // fungsi untuk mengacak number
+  function acakPemabahan(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  // fungsi untuk menentukan total umur manusia. menurut penelitian WHO pada 2016, rata-rata umur laki = 69.8 (70), wanita 74.2 (74).
+  // source WHO, berdasarkan google :)
+  function jumlahUmur(gender) {
+    let start;
+    if (gender == "laki") {
+      start = 70;
+    } else {
+      start = 74;
+    }
+
+    // Bonus tahun
+    total_tahun = start + acakPemabahan(Math.random() < 0.5 ? -10 : 10);
+
+    return total_tahun;
+  }
+
+  function ulangi() {
+    $(".hasil_prediksi").html("");
+  }
+
+  $("#ramalan-kematian").on("click", ".btn-ramalan", function () {
+    $(".text-eror").remove();
+    // inisialisasi attribute. diambil dari form diatas
+    let tahunini = new Date().getFullYear();
+    console.log(tahunini);
+    let nama = $(".nama").val();
+    let kelahiran = $(".tahun_lahir").val();
+    let gender = $(".gender:checked").val();
+
+    // inisialiasi sekaligus mengacak angka nya langsung
+    let frek_rokok = acakPemabahan(parseInt($("#frek_rokok").val()));
+    let tidur = acakPemabahan(parseInt($("#tidur").val()));
+    let alkohol = acakPemabahan(parseInt($("#alkohol").val()));
+    let olahraga = acakPemabahan(parseInt($("#olahraga").val()));
+    let stress = acakPemabahan(parseInt($("#stress").val()));
+
+    if (nama == "" || nama.length < 7 || nama.length > 25) {
+      ShowErrText(
+        ".nama",
+        "Periksa Kembali ! <b>(Min 7 - Max 27 Karakter)</b>"
+      );
+    } else if (
+      kelahiran == "" ||
+      kelahiran.match(/\D/) ||
+      kelahiran.length < 4 ||
+      kelahiran.length > 4 ||
+      kelahiran < 1
+    ) {
+      ShowErrText(".tahun_lahir", "(<b>Hanya Angka</b>, Min Max 4 Karakter)");
+    } else if (parseInt(kelahiran) > tahunini) {
+      ShowErrText(
+        ".tahun_lahir",
+        "(<b>Jangan Main-Main</b>, Sekarang Masih Tahun " + tahunini + ")"
+      );
+    } else if (gender === undefined) {
+      ShowErrText(".alert-gender", "<b>Gender</b>, Harus Dipilih !");
+    } else if (Number.isNaN(frek_rokok)) {
+      ShowErrText("#frek_rokok", "Pilihan Tidak Boleh Kosong !");
+    } else if (Number.isNaN(tidur)) {
+      ShowErrText("#tidur", "Pilihan Tidak Boleh Kosong !");
+    } else if (Number.isNaN(alkohol)) {
+      ShowErrText("#alkohol", "Pilihan Tidak Boleh Kosong !");
+    } else if (Number.isNaN(olahraga)) {
+      ShowErrText("#olahraga", "Pilihan Tidak Boleh Kosong !");
+    } else if (Number.isNaN(stress)) {
+      ShowErrText("#stress", "Pilihan Tidak Boleh Kosong !");
+    } else {
+      // Semua data angka kita masukan kedalamm array, agar bisa dilooping
+      let arr_angka = [frek_rokok, tidur, alkohol, olahraga, stress];
+
+      // Mari kita mulai proses!!!
+      let start_umur = jumlahUmur(gender);
+      for (let i = 0; i < arr_angka.length; i++) {
+        // jika total jumlah umur dibawah 30, keluar looping
+        if (start_umur < 30) {
+          break;
+        }
+        start_umur += arr_angka[i];
+      }
+
+      let hasil_prediksi = parseInt(kelahiran) + start_umur;
+
+      $("#ramalan-kematian").hide();
+
+      // animasi loading duls
+      loadingduls(
+        "#hasil-ramalan-kematian",
+        "Meramalkan Kematian Mu",
+        40,
+        true
+      );
+
+      var OutHasil = `<h4>Hasil Ramalan kami<br/>menyatakan bahwa</h4>`;
+      OutHasil += `<h2 class="horror-text">${nama}</h2>`;
+      OutHasil += `<h4>yang lahir di tahun</h4>`;
+      OutHasil += `<h2 class="horror-text">${kelahiran}</h2>`;
+      OutHasil += `<h4>akan meninggal<br/>pada tahun</h4>`;
+      OutHasil += `<h2 class="horror-text">${hasil_prediksi}</h2>`;
+      $("#kematianku").html(OutHasil);
+      $("#ramalan-kematian").trigger("reset");
+    }
+  });
+});
