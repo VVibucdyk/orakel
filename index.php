0d4f728cb0cf537d78601e4762dad52d20e5bad2
difawitsqard <difawitsqard@gmail.com>
2022-08-06 19:38:16 +0700

    Penambahan Sambutan

--- index.php
+++ index.php
@@ -52,12 +52,10 @@ Edit By Difa WRD 10121919 & Stefani Olga 10121908
   </noscript>
 </head>
 
-
-
 <di style="top: 0px;" class="topnav">
   <div class="topnav-right">
     <?php if ($islogin) { ?>
-      <a href="public/keluar.php">Logout</a>
+      <a href="public/keluar">Logout</a>
     <?php } else { ?>
       <a onclick="Open('public/register');">Daftar</a>
       <a onclick="Open('public/masuk');">Masuk</a>
@@ -70,83 +68,91 @@ Edit By Difa WRD 10121919 & Stefani Olga 10121908
   <div id="wrapper">
     <!-- Header -->
     <header id="header">
-
-      <div class="content">
-        <div class="inner">
-          <div id="magic-title">
-            <h1>SELAMAT DATANG</h1>
-            <h2>DI GERBANG OSIRIS</h2>
-            <p><b>"MEREKA BILANG JANGAN TAKUT!!!"</b></p>
-          </div>
+     <?php if ($islogin) { ?>
+      <div class="user-info">
+        <h4>Hai</h4>
+        <h3><?= $NamaKu ?></h3>
+      </div>
+    <?php } ?>
+    <div class="content">
+      <div class="inner">
+        <div id="magic-title">
+          <h1>SELAMAT DATANG</h1>
+          <h2>DI GERBANG OSIRIS</h2>
+          <p><b>"MEREKA BILANG JANGAN TAKUT!!!"</b></p>
         </div>
       </div>
-
-      <nav>
-        <ul>
-          <li>
-            <div class="dropdown">
-              <a class="dropbtn" id="artikel">Artikel
-                <i class="fa fa-caret-down"></i>
-              </a>
-              <div class="dropdown-content">
-                <div class="wrapper">
-                  <?php listGenreIndex() ?>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="dropdown">
-              <a class="dropbtn">Game<i class="fa fa-caret-down"></i></a>
-              <div class="dropdown-content">
-                <a onclick="Open('public/ritual');">Ritual</a>
-                <a onclick="Open('public/tanyadozzkiller');">Tanya</a>
-                <a onclick="Open('public/ramalankematian');">Kematian</a>
-              </div>
-            </div>
-          </li>
-          <?php if ($islogin) { ?>
-            <li>
-              <a class="magic-title" data-deskripsi="Edit Ceritamu Bla Bla Bla Bla Bla Bla Bla Bla Bla Slebew" onclick="Open('public/sejarah');">
-                Edit
-              </a>
-            </li>
-            <li>
-              <a class="magic-title" data-deskripsi="Posting cerita mu disini !!! Pengalaman - Kejadian nyata - Mitos - Misteri di sekitar mu. BAGIKAN DISINI JIKA ANDA BERANI !!!" onclick="Open('public/posting_cerita');">
-                Posting
-              </a>
-            </li>
-          <?php } ?>
-          <li><a onclick="Open('public/team');">Credit</a></li>
-        </ul>
-      </nav>
-
-    </header>
-
-    <!-- Main -->
-    <div id="main">
-      <article id="pages"></article>
     </div>
 
-    <!-- Footer -->
-    <footer id="footer">
-      <p class="seehay">HANYA FIKTIF BELAKA !</p>
-    </footer>
+    <nav>
+      <ul>
+        <li>
+          <div class="dropdown">
+            <a class="dropbtn" id="artikel">Artikel
+              <i class="fa fa-caret-down"></i>
+            </a>
+            <div class="dropdown-content">
+              <div class="wrapper">
+                <?php listGenreIndex() ?>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="dropdown">
+            <a class="dropbtn">Game<i class="fa fa-caret-down"></i></a>
+            <div class="dropdown-content">
+              <a onclick="Open('public/ritual');">Ritual</a>
+              <a onclick="Open('public/tanyadozzkiller');">Tanya</a>
+              <a onclick="Open('public/ramalankematian');">Kematian</a>
+            </div>
+          </div>
+        </li>
+        <?php if ($islogin) { ?>
+          <li>
+            <a class="magic-title" data-deskripsi="Posting cerita mu disini !!! Pengalaman - Kejadian nyata - Mitos - Misteri di sekitar mu. BAGIKAN DISINI JIKA ANDA BERANI !!!" onclick="Open('public/posting_cerita');">
+              Posting
+            </a>
+          </li>
+          <li>
+            <a class="magic-title" data-deskripsi="Menu ini untuk menghapus atau memperbarui postingan kamu. Hati Hati Menggunakan Menu ini, postingan yang sudah dihapus tidak dapat dikembalikan !" onclick="Open('public/artikelku');">
+              Kelola
+            </a>
+          </li>
+        <?php } ?>
+        <li>
+          <a onclick="Open('public/tentang');">Tentang</a>
+        </li>
+      </ul>
+    </nav>
+
+  </header>
+
+  <!-- Main -->
+  <div id="main">
+    <article id="pages"></article>
   </div>
 
-  <!-- BG -->
-  <div id="bg"></div>
+  <!-- Footer -->
+  <footer id="footer">
+    <p class="seehay">©2022 By <a onclick="Open('public/team');">Uraa Team</a></p>
+  </footer>
+</footer>
+</div>
 
-  <!-- Scripts -->
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/browser.min.js"></script>
-  <script src="assets/js/breakpoints.min.js"></script>
-  <script src="assets/js/util.js"></script>
-  <script src="assets/js/main.js"></script>
-  <script src="assets/js/iziToast.min.js"></script>
-  <script src="_URAA/js/main-costum.js"></script>
-  <script src="_URAA/js/costum.js"></script>
-  <script src="assets/js/ckeditor.js"></script>
+<!-- BG -->
+<div id="bg"></div>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/browser.min.js"></script>
+<script src="assets/js/breakpoints.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/iziToast.min.js"></script>
+<script src="_URAA/js/main-costum.js"></script>
+<script src="_URAA/js/costum.js"></script>
+<script src="assets/js/ckeditor.js"></script>
 
 </body>
 
