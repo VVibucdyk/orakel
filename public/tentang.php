d1ab549c39132953b4ccc0c6416c22ade5699f18
difawitsqard <difawitsqard@gmail.com>
2022-08-07 22:04:22 +0700

    disini teh @StefaniOlgaga editnya

---
+++ public/tentang.php
@@ -0,0 +1,5 @@
+
+<h2 class="major">Tentang</h2>
+<p>
+    Web Interaktif berisi game tentang Ramalan Kematian dan Cerita-Cerita Misteri yang akan mengundang rasa penasaran. Para member juga dapat memposting cerita misteri disini. Jika kamu berani,LAKUKAN DISINI!!!
+</p>
