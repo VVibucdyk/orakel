883f181185599c688ed4a18c6e1c877d7e849f70
difawitsqard <difawitsqard@gmail.com>
2022-08-10 05:43:24 +0700

    + gambar di page tentang ( siapa tau kepake )

--- public/tentang.php
+++ public/tentang.php
@@ -1,5 +1,7 @@
-
 <h2 class="major">Tentang</h2>
+<span class="image main">
+    <img src="_URAA/images/typing-uraa.jpg" alt="typing" />
+</span>
 <p>
     Web Interaktif berisi game tentang Ramalan Kematian dan Cerita-Cerita Misteri yang akan mengundang rasa penasaran. Para member juga dapat memposting cerita misteri disini. Jika kamu berani,LAKUKAN DISINI!!!
 </p>
