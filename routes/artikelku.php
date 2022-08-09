757b4dcdcd99d0046b3d4cf4a5bae0f2e9a5e811
difawitsqard <difawitsqard@gmail.com>
2022-08-10 05:42:57 +0700

    fix salah pake fungsi filter

--- routes/artikelku.php
+++ routes/artikelku.php
@@ -3,18 +3,18 @@
 // Created By : Difa Witsqa RD 
 // Date created : 06 / 08 / 2022
 
-if(!isSessionValid()) exit("Direct access not permitted.");
+if (!isSessionValid()) exit("Direct access not permitted.");
 
 $nama_user = getUserInfo('nama');
 $username_user = getUserInfo('username');
 
 $valid = false;
 $element = "result-box";
-$msg = "Kesalahan Tidak Di Ketahui"; 
+$msg = "Kesalahan Tidak Di Ketahui";
 
 // menangkap data post & menyesuaikan sesuai perintah value
 $command_delete = isset($_POST['delete']) ? filterhtml($_POST['delete']) : null;
-$command_edit = isset($_POST['edit']) ? filterhtml($_POST['edit']) : null; 
+$command_edit = isset($_POST['edit']) ? filterhtml($_POST['edit']) : null;
 
 $p_artikel_code = $command_delete != null ? $command_delete : ($command_edit != null ? $command_edit : null);
 
@@ -23,8 +23,8 @@ if ($p_artikel_code != null) {
     $artikelku->execute([$p_artikel_code]);
     $data = $artikelku->fetch();
 
-    if($data == 0) {
-        $msg = "Kode Artikel Tidak Valid !";   
+    if ($data == 0) {
+        $msg = "Kode Artikel Tidak Valid !";
     } else {
 
         $kode_artikel = $data['kode_artikel'];
@@ -33,8 +33,8 @@ if ($p_artikel_code != null) {
         $genre_id = $data['genre_id'];
         $url_konten = "../_URAA/images/konten/{$username_user}/{$kode_artikel}";
 
-    // Perintah Hapus Artikel
-        if($command_delete != null) {
+        // Perintah Hapus Artikel
+        if ($command_delete != null) {
             $DeletImage = true;
 
             if (file_exists($url_konten)) {
@@ -50,24 +50,24 @@ if ($p_artikel_code != null) {
             } else {
                 $msg = 'Terjadi Error Hubungi Developer !';
             }
-    // Perintah Update Artikel
-        } else if($command_edit != null) {
-            $p_judul = isset($_POST['judul']) ? clean($_POST['judul']) : '';
-            $p_genre = isset($_POST['genre']) ? clean($_POST['genre']) : '';
+            // Perintah Update Artikel
+        } else if ($command_edit != null) {
+            $p_judul = isset($_POST['judul']) ? filterhtml($_POST['judul']) : '';
+            $p_genre = isset($_POST['genre']) ? filterhtml($_POST['genre']) : '';
             $p_artikel = isset($_POST['artikel']) ? $_POST['artikel'] : '';
 
-            if(empty($p_judul) || strlen($p_judul) < 5 || strlen($p_judul) > 125) {
+            if (empty($p_judul) || strlen($p_judul) < 5 || strlen($p_judul) > 125) {
                 $element = "judul";
-                $msg = "(Min 5 - Maks 125 Karakter)";     
-            } else if(empty($p_genre)) {
+                $msg = "(Min 5 - Maks 125 Karakter)";
+            } else if (empty($p_genre)) {
                 $element = "genre";
-                $msg = "Genre Cerita Harus Di Isi !"; 
-            } else if(empty($p_artikel) || strlen($p_artikel) < 5 || strlen($p_artikel) > 5000) {
+                $msg = "Genre Cerita Harus Di Isi !";
+            } else if (empty($p_artikel) || strlen($p_artikel) < 5 || strlen($p_artikel) > 15000) {
                 $element = "artikel";
-                $msg = "(Min 5 - Maks 5000 Karakter)"; 
-            } else if($p_genre == $genre_id && $p_judul == $judul_artikel && md5($p_artikel)==md5($isi_artikel)) {
+                $msg = "(Min 5 - Maks 15000 Karakter)";
+            } else if ($p_genre == $genre_id && $p_judul == $judul_artikel && md5($p_artikel) == md5($isi_artikel)) {
                 $element = "artikel";
-                $msg = "Whoop Kamu Belum Merubah Apapun ?";  
+                $msg = "Whoop Kamu Belum Merubah Apapun ?";
             } else {
                 $UpdateArtikel = $conn->prepare("UPDATE table_artikel SET judul_artikel=?, genre_id=?, isi_artikel=?, tgl_publish=? WHERE kode_artikel=?");
                 $simpan = $UpdateArtikel->execute([$p_judul, $p_genre, $p_artikel, $today, $kode_artikel]);
@@ -78,19 +78,18 @@ if ($p_artikel_code != null) {
                     $msg = 'Err DB Hubungi Developer !';
                 }
             }
-        } 
+        }
     }
 }
 
 if (isset($msg)) {
     $send = new stdClass();
-    $send->status=$valid;
-    $send->info=new stdClass();
-    isset($element) ? $send->info->elementid=$element : ''; 
-    $send->info->msg=$msg; 
+    $send->status = $valid;
+    $send->info = new stdClass();
+    isset($element) ? $send->info->elementid = $element : '';
+    $send->info->msg = $msg;
     echo json_encode($send);
 } else {
     http_response_code(400);
     exit("Direct access not permitted.");
 }
-?>
