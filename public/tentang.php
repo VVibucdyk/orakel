5eea674d7558abb16da4262fcea5440848296d29
StefaniOlgaga <olgaukarop@gmail.com>
2022-08-10 22:06:20 +0700

    TENTANG

--- public/tentang.php
+++ public/tentang.php
@@ -3,5 +3,5 @@
     <img src="_URAA/images/typing-uraa.jpg" alt="typing" />
 </span>
 <p>
-    Web Interaktif berisi game tentang Ramalan Kematian dan Cerita-Cerita Misteri yang akan mengundang rasa penasaran. Para member juga dapat memposting cerita misteri disini. Jika kamu berani,LAKUKAN DISINI!!!
+    Web Interaktif berisi game tentang Ramalan Kematian dan Cerita-Cerita Misteri yang akan mengundang rasa penasaran. Para member dapat berbagi cerita misteri disini dan bermain mini game yang sudah tersedia seperti Ramalan Kematian dan Tanya Dozz Killer. Jangan hanya membaca, tapi BUKTIKAN bahwa kamu BERANI. LAKUKAN SEKARANG DISINI!!!
 </p>
